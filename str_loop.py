d88874f317ed98632e651b2070631d1cfb320cea
dikshantnandankar <darshan.nandankar3@gmail.com>
2026-03-18 12:59:35 +0530

    List for loop example

--- str_loop.py
+++ str_loop.py
@@ -1,1 +1,6 @@
 print(2+2)
+arc_address = 'Gurudev Nagar Main Rd, beside HPCL Gurudeo Petrol Pump, Chota Tajbag, Nandanvan, Nagpur, Maharashtra 440024'
+
+for ch in arc_address:
+    print(f'ch = {ch}')
+    print('-' *5)
